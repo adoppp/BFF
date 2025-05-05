8c3827cbd337b7a66899a5b84f59d5a72dd0a624
adop <chdn786od@gmail.com>
2025-05-05 17:15:31 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import { BrowserRouter } from 'react-router-dom';
+//import { Provider } from 'react-redux';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="bff-teams-creator">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
